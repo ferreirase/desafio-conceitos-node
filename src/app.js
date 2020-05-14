13427d8ab1d4a4f0339986066cf90558a85c173e
Anderson Raphael Ferreira <phaell.anderson09@gmail.com>
2020-05-14 10:54:19 -0300

    feature(project): project finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,68 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+// listar todos os repos
+app.get('/repositories', (request, response) =>
+  response.status(200).send(repositories)
+);
+
+//criar novo repo
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const newRepo = { id: uuid(), title, techs, url, likes: 0 };
+
+  repositories.push(newRepo);
+
+  return response.status(201).json(newRepo);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+//atualizar repos
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const repoExists = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoExists < 0) {
+    return response.status(400).json();
+  }
+
+  repositories[repoExists].title = title;
+  repositories[repoExists].url = url;
+  repositories[repoExists].techs = techs;
+
+  return response.status(200).json(repositories[repoExists]);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+//deletar um repositório
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const repoExists = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoExists < 0) {
+    return response.status(400).json();
+  }
+
+  repositories.splice(repoExists, 1);
+
+  return response.status(204).json();
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+//adicionar like nos repos
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repoExists = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoExists < 0) {
+    return response.status(400).json();
+  }
+
+  repositories[repoExists].likes = repositories[repoExists].likes + 1;
+
+  return response.status(200).json({ likes: repositories[repoExists].likes });
 });
 
 module.exports = app;
